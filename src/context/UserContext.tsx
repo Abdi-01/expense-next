65b452768bbdd3c87daadb584700f53181dc5e0c
Abdi-01 <abdialghi@gmail.com>
2024-08-06 10:42:00 +0700

    add keeplogin

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 import * as React from "react";
 import { UserContextType, UserType } from "./types";
+import { apiCall } from "@/helper/axiosInstance";
 
 // Create context with default value
 export const UserContext = React.createContext<UserContextType>({
@@ -16,6 +17,27 @@ const UserProvider: React.FunctionComponent<IUserProviderProps> = ({
   children,
 }) => {
   const [user, setUser] = React.useState<UserType | null>(null);
+
+  const keepLogin = async () => {
+    try {
+      const { data } = await apiCall.get("/auth/keeplogin", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("auth")}`,
+        },
+      });
+
+      localStorage.setItem("auth", data.result.token);
+      setUser({ email: data.result.email, noTelp: data.result.noTelp });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      keepLogin();
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
